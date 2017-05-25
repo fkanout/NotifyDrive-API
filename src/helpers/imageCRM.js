18dcb67f29a06a2244825f59ce6a7143b89a3e9a
Faisal KANOUT <f.kanout@gmail.com>
2017-05-25 22:32:01 +0200

    UPD-Working Google Vision with http request

--- src/helpers/imageCRM.js
+++ src/helpers/imageCRM.js
@@ -8,10 +8,21 @@ const visionClient = Vision({
   keyFilename: './crm_config.json'
 });
 
+const rpn = require('request-promise-native');
+
 // Performs label detection on the image file
 const imageCRM = async (image) =>{
     try{
-        return await visionClient.detectText(image);
+        return await rpn.post({
+            url: 'https://vision.googleapis.com/v1/images:annotate',
+            qs: {
+                'key': 'AIzaSyCwSEDGhFvlALLReFPDT0TfzZ4760ic6gw'
+            },
+            headers: {
+                'Content-Type': 'application/javascript',
+            },
+            body: JSON.stringify(body)
+        });
     }catch(err){
         console.log(err);
 
