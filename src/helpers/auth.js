db4d93c714c6433be92a98c784c621cd404a3a78
Faisal KANOUT <f.kanout@gmail.com>
2017-09-06 12:28:39 +0200

    ESLINT

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,20 +1,17 @@
-/**
- * Created by javascript on 01/10/2016.
- */
-'use strict';
 const Joi = require('joi');
 const jwt = require('./jwt');
+
 const authSchema = Joi.object().keys({
-    accessToken: [Joi.string(), Joi.number()],
+  accessToken: [Joi.string(), Joi.number()],
 });
 
-module.exports.authenticate = async function (token) {
-    Joi.validate({ accessToken: token }, authSchema,  (err) => false);
-    let userId = '';
-    try{
-       userId = jwt.verify(token);
-    } catch (err){
-       return false;
-    }
-    return userId;
+module.exports.authenticate = async (token) => {
+  Joi.validate({ accessToken: token }, authSchema, err => false);
+  let userId = '';
+  try {
+    userId = jwt.verify(token);
+  } catch (err) {
+    return false;
+  }
+  return userId;
 };
