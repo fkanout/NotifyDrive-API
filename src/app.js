e60a7259868e44c7c951877190a61caad009eaea
Faisal KANOUT <f.kanout@gmail.com>
2017-09-06 12:47:46 +0200

    ADD - new packegs to debug

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@ require('./db/connection');
 
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const logger = require('koa-logger');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
 const router = require('./routes');
+const cors = require('./helpers/cors');
 
-const app = new Koa();
-app.use(bodyParser({}));
-router.map(el => app.use(el.routes()));
-app.listen(3003);
+(() => {
+  const app = new Koa();
+  onerror(app);
+  app.use(bodyParser({}))
+    .use(json())
+    .use(logger())
+    .use(cors());
+  router.map(el => app.use(el.routes()));
+  app.listen(3003);
+})();
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,17 +3,12 @@ const router = require('koa-router')();
 const fsHelper = require('../helpers/fs');
 
 fsHelper.getFiles(__dirname).filter(el => (el !== path.basename(__filename) && path.extname(el) === '.js')).forEach(el => require(path.join(__dirname, el))(router));
-
 const routes = fsHelper.getDirectories(__dirname);
-
 const routers = [];
 
 routes.forEach((el) => {
   const req = require(path.join(__dirname, el));
   if (typeof req === 'object' && req.length) { req.forEach(r => routers.push(r)); } else { routers.push(req); }
-},
-);
-
+});
 routers.push(router);
-
 module.exports = routers;
