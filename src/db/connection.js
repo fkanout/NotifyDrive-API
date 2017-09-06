411c61d51d022d0a0c3c7a7e193be07417ee9b67
Faisal KANOUT <f.kanout@gmail.com>
2017-09-06 12:27:38 +0200

    FIX - MongoDB warning msgs

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,29 +4,33 @@
 
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+
 // Create the database connection
-mongoose.connect(process.env.MONGO_DB_URL);
+mongoose.connect(process.env.MONGO_DB_URL, { useMongoClient: true });
 
 // CONNECTION EVENTS
 // When successfully connected
-mongoose.connection.on('connected', function () {
-    console.log('Mongoose connection open to ' + process.env.MONGO_DB_URL);
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connection open to ${process.env.MONGO_DB_URL}`);
 });
 
 // If the connection throws an error
-mongoose.connection.on('error',function (err) {
-    console.log('Mongoose connection error: ' + err);
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose connection error: ${err}`);
 });
 
 // When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose connection disconnected');
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose connection disconnected');
 });
 
 // If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-    mongoose.connection.close(function () {
-        console.log('Mongoose connection disconnected through app termination');
-        process.exit(0);
-    });
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose connection disconnected through app termination');
+    process.exit(0);
+  });
 });
+
+mongoose.pro;
