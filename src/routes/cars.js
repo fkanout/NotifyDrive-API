702d55dd05d08dcd610f4328bb34af49211617d3
Faisal KANOUT <f.kanout@gmail.com>
2017-09-06 12:30:12 +0200

    UPD - BIG routes refactor

---
+++ src/routes/cars.js
@@ -0,0 +1,109 @@
+
+
+const auth = require('../helpers/auth');
+const Car = require('../db/models/car');
+
+module.exports = (router) => {
+  router.get(
+    '/cars',
+    async (ctx, next) => {
+      const user = await auth.authenticate(ctx.request.headers.authorization);
+      ctx.assert(user, 401);
+      const cars = await Car.find({ owner: user.userId });
+      ctx.assert(cars, 409);
+      ctx.body = cars;
+      await next;
+    },
+  );
+  router.get(
+    '/cars/:plateNumber',
+    async (ctx, next) => {
+      const userId = await auth.authenticate(ctx.request.headers.authorization);
+      ctx.assert(userId, 401);
+      // todo add Joi to validate body
+      const plate = await Car.findOne({ plate: ctx.params.plateNumber });
+      ctx.assert(plate, 404);
+      ctx.body = plate;
+      await next;
+    },
+  );
+  router.post(
+    '/cars/:plateNumber',
+    async (ctx, next) => {
+      const user = await auth.authenticate(ctx.request.headers.authorization);
+      ctx.assert(user, 401);
+      const car = await Car.findOne({ plate: ctx.request.body.plate });
+      ctx.assert(!car, 409);
+      // todo add Joi to validate body
+      const newCarToAdd = await Car.create({
+        plate: ctx.request.body.plate,
+        mark: ctx.request.body.mark,
+        year: ctx.request.body.year,
+        model: ctx.request.body.model,
+        owner: user.userId,
+        department: ctx.request.body.department,
+      });
+      ctx.assert(newCarToAdd, 500);
+      ctx.body = newCarToAdd;
+      await next;
+    },
+  );
+  router.post(
+    '/cars/oscaro/:plateNumber',
+    async (ctx, next) => {
+      const user = await auth.authenticate(ctx.request.headers.authorization);
+      ctx.assert(user, 401);
+      const options = {
+        method: 'POST',
+        uri: ' https://www.oscaro.com/Catalog/SearchEngine/LicencePlateJQueryV2',
+        body: {
+          frenchLicencePlate: ctx.request.body.plate,
+        },
+        json: true, // Automatically stringifies the body to JSON
+      };
+      const requestedCar = await rpn(options);
+      ctx.assert(requestedCar, 401);
+      const group = requestedCar.types[0].Text.split(' ');
+      ctx.body = {
+        Mark: group[0],
+        Model: group[1],
+      };
+      await next;
+    },
+  );
+
+  // ******* This route to re-thinking 
+  router.post(
+    '/cars/evaluation/:plateNumber',
+    async (ctx, next) => {
+      const user = await auth.authenticate(ctx.request.headers.authorization);
+      ctx.assert(user, 401);
+
+      const carId = ctx.request.body.carId;
+      const ownerId = ctx.request.body.ownerId;
+      let car;
+
+      // todo Optimisation;
+      if (ctx.request.body.evaluation === 'positive') {
+        car = await Car.update(
+          { _id: carId },
+          { $addToSet: {
+            'evaluation.positive': user.userId,
+          },
+          });
+      } else {
+        car = await Car.update(
+          { _id: carId },
+          { $addToSet: {
+            'evaluation.negative': user.userId,
+          },
+          });
+      }
+      ctx.assert(car, 404, 'Car not found');
+      ctx.body = car;
+      await next;
+    },
+  );
+  // *************
+};
+
---
+++ src/routes/signin.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcrypt');
+const jwt = require('../helpers/jwt');
+const User = require('../db/models/user');
+const Device = require('../db/models/device');
+
+module.exports = (router) => {
+  router.post('/signin',
+    async (ctx, next) => {
+      const user = await User.findOne({ email: ctx.request.body.email });
+      console.log('user', user);
+      ctx.assert(user, 401);
+      const password = bcrypt.compareSync(ctx.request.body.password, user.password);
+      ctx.assert(password, 401, 'Wrong password');
+      const token = jwt.sign({ userId: user._id });
+      ctx.assert(token, 500, 'Token problem');
+      const deviceQuery = { deviceInfo: { uuid: ctx.request.body.device.uuid } };
+      const device = await Device.findOneAndUpdate(deviceQuery, { token: ctx.request.body.deviceToken,
+        deviceInfo: ctx.request.body.device,
+        userId: user._id,
+      }, { new: true, upsert: true });
+      console.log(device);
+      ctx.body = { token };
+      await next();
+    });
+
+  router.get(
+    '/checktoken',
+    async (ctx, next) => {
+      const authorization = ctx.request.headers.authorization;
+      ctx.assert(authorization, 401, 'Not correct header');
+      try {
+        await jwt.verify(authorization);
+        ctx.body = { success: true };
+      } catch (err) {
+        ctx.assert(null, 401, 'Not valid token');
+      }
+      await next;
+    },
+  );
+};
+
