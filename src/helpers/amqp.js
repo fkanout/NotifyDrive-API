ce09414f9a5fc0ab8ada8414a8df8c5ee40b921e
Faisal KANOUT <f.kanout@gmail.com>
2017-09-06 12:29:04 +0200

    UPD - disable RabbitMQ

--- src/helpers/amqp.js
+++ src/helpers/amqp.js
@@ -2,19 +2,19 @@
  * Created by JS on 25/12/2016.
  */
 
-const open = require('amqplib').connect(process.env.AMQP);
+// const open = require('amqplib').connect(process.env.AMQP);
 
-const publish = async function(data, queue){
-    console.log(data, queue);
-    open.then(function (conn){
-        let ok = conn.createChannel();
-        ok = ok.then(function (ch){
-            ch.assertQueue(queue);
-            ch.sendToQueue(queue, new Buffer(data));
-        });
-        return ok;
-    }).then(null, console.warn);
-};
+// const publish = async function(data, queue){
+//     console.log(data, queue);
+//     open.then(function (conn){
+//         let ok = conn.createChannel();
+//         ok = ok.then(function (ch){
+//             ch.assertQueue(queue);
+//             ch.sendToQueue(queue, new Buffer(data));
+//         });
+//         return ok;
+//     }).then(null, console.warn);
+// };
 
 
-module.exports.publish = publish;
+// module.exports.publish = publish;
