db939516250c1f68253576bfabd28d1acddc3a6d
Faisal KANOUT <f.kanout@gmail.com>
2017-09-10 20:47:27 +0200

    ADD - RabbitMQ Test

--- test/connection.spec.js
+++ test/connection.spec.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 const chai = require('chai');
 
 const expect = chai.expect;
+const should = chai.should();
 
+const amqp = require('../lib/helpers/amqp');
 
 describe('MongoDB', () => {
   it('Should NOT connect to MongoDB with no/wrong URL porvided', (done) => {
@@ -22,5 +24,19 @@ describe('MongoDB', () => {
       expect(connected).to.equal(true);
       done();
     });
+  });
+});
+describe('RabbitMQ', () => {
+  const q = 'testQ';
+  it('Should NOT connect to RabbitMQ with wrong URL', (done) => {
+    require('amqplib').connect()
+      .then()
+      .catch((err) => {
+        should.exist(err);
+        done();
+      });
+  });
+  it('Should sent RabbitMQ msg via TEST queue', (done) => {
+    amqp.publish('TEST', q).then(() => done());
   });
 });
